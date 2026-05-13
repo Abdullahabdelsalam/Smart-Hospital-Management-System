c6d5e2f226170b5ed5569eee1736b10c7d491f5f
Abdallah Abdalslam <abdullahabdalslam705@gmail.com>
2026-05-13 22:55:26 +0300

    init commit

---
+++ src/main/java/com/abdullah/util/HibernateUtil.java
@@ -0,0 +1,18 @@
+package com.abdullah.util;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateUtil {
+    private static final SessionFactory factory;
+
+    static {
+        factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .buildSessionFactory();
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return factory;
+    }
+}
---
+++ src/test/java/com/abdullah/smart_hospital_management/SmartHospitalManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdullah.smart_hospital_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartHospitalManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
